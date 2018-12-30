27b8c32055d01eac881e2815e326c6340e70c074
Jignesh Patel <patel.jignesh705@gmail.com>
2018-12-30 21:41:10 +0530

    Added sample scene navigation

---
+++ Don't Panic/Assets/Scripts/GoToControlRoom.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToControlRoom : MonoBehaviour {
+
+	//public GameObject door;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void goOut() {
+		Debug.Log ("Go out");
+		SceneManager.LoadScene ("Main");
+	}
+}
