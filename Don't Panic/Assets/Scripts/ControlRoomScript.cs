8d13d9e63c22546783042e50ec1abeda5f445b9a
Jignesh Patel <patel.jignesh705@gmail.com>
2019-01-04 02:08:52 +0530

    Added HUD progress slider to control room

--- Don't Panic/Assets/Scripts/ControlRoomScript.cs
+++ Don't Panic/Assets/Scripts/ControlRoomScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ControlRoomScript : MonoBehaviour {
 
@@ -13,16 +14,34 @@ public class ControlRoomScript : MonoBehaviour {
 
 	public ParticleSystem destroyEffect;
 
+	public Canvas sliderCanvas;
+
+	public Slider slider;
+
 	private Material ogMat;
+	private bool pointerFlag = false;
+	private float enterTime;
+	private float hoverTime = 1.5f;
+	private GameObject focusedObj;
 
 	// Use this for initialization
 	void Start () {
-		
+		slider.maxValue = hoverTime;
+		sliderCanvas.gameObject.SetActive (false);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (pointerFlag) {
+			//Sets the progress slider value
+			float sliderValue = Time.time - enterTime;
+			slider.value = sliderValue;
+
+			if ((Time.time - enterTime) > hoverTime) {
+				CollectArtwork (focusedObj);
+				pointerFlag = false;
+			}
+		}
 	}
 
 	public void SwitchOffWarning() {
@@ -36,15 +55,33 @@ public class ControlRoomScript : MonoBehaviour {
 		destroyEffect.Play ();
 		StaticValues.CollectibleScore = 1;
 		Debug.Log (StaticValues.CollectibleScore);
+		PointerExit ();
 	}
 
 	public void HighlightObj(GameObject obj) {
-		ogMat = obj.GetComponent<MeshRenderer> ().material;
+		focusedObj = obj;
+		PointerEnter ();
 
+		ogMat = obj.GetComponent<MeshRenderer> ().material;
 		obj.GetComponent<MeshRenderer> ().material = highlightMat;
 	}
 
 	public void UnhighlightObj(GameObject obj) {
+		PointerExit ();
+
 		obj.GetComponent<MeshRenderer> ().material = ogMat;
+	}
+
+	private void PointerEnter() {
+		sliderCanvas.gameObject.SetActive (true);
+		pointerFlag = true;
+		enterTime = Time.time;
+	}
+
+	private void PointerExit() {
+		pointerFlag = false;
+		sliderCanvas.gameObject.SetActive (false);
+		//Reset progress slider value
+		slider.value = 0.0f;
 	}
 }
